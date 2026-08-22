f9c23c72c5921d86b93d6d20cb5c2d1e67b0588a
Enzo11 <enzo11@users.noreply.github.com>
2026-08-22 15:18:29 +0530

    fix: next.js manifest type error

--- frontend/client/src/app/manifest.ts
+++ frontend/client/src/app/manifest.ts
@@ -37,19 +37,19 @@ export default async function manifest(): Promise<MetadataRoute.Manifest> {
         src: appIcon,
         sizes: '192x192',
         type: 'image/png',
-        purpose: 'any maskable'
+        purpose: 'maskable'
       },
       {
         src: appIconLarge,
         sizes: '384x384',
         type: 'image/png',
-        purpose: 'any maskable'
+        purpose: 'maskable'
       },
       {
         src: appIconLarge,
         sizes: '512x512',
         type: 'image/png',
-        purpose: 'any maskable'
+        purpose: 'maskable'
       }
     ],
     shortcuts: [
